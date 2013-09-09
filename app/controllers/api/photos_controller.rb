db773cce7923b6fac7c240e5da005f7ef7a31411
Mario A Chavez <mario.chavez@gmail.com>
2013-09-09 20:28:43 +0000

    Filter resources by current user passed with token

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -3,9 +3,14 @@ module Api
     before_action :get_auth_header
 
     def index
-      @photos = Photo.get_by_user_eager(1).desc_id_ordered
+      @photos = Photo.get_by_user_eager(current_user.id).desc_id_ordered
 
       respond_with @photos
+    end
+
+    private
+    def current_user
+      @authorization.user
     end
   end
 end
